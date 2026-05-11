b038fe3e238f70046a7f5d65fa615401d9183236
SoeW0 <pa89218@gmail.com>
2026-05-11 19:51:40 -0300

    raylib initial setup

---
+++ CMakeFiles/tictactoe.dir/src/main.cpp.o.d
@@ -0,0 +1,53 @@
+CMakeFiles/tictactoe.dir/src/main.cpp.o: \
+ /home/pedro/Projects/tictactoe-cpp/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/pedro/Projects/tictactoe-cpp/_deps/raylib-src/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdarg.h \
+ /usr/include/c++/16.1.1/array /usr/include/c++/16.1.1/compare \
+ /usr/include/c++/16.1.1/bits/version.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/16.1.1/pstl/pstl_config.h \
+ /usr/include/c++/16.1.1/concepts /usr/include/c++/16.1.1/type_traits \
+ /usr/include/c++/16.1.1/initializer_list \
+ /usr/include/c++/16.1.1/bits/stdexcept_throw.h \
+ /usr/include/c++/16.1.1/bits/exception_defines.h \
+ /usr/include/c++/16.1.1/bits/stdexcept_throwfwd.h \
+ /usr/include/c++/16.1.1/bits/stl_algobase.h \
+ /usr/include/c++/16.1.1/bits/cpp_type_traits.h \
+ /usr/include/c++/16.1.1/ext/type_traits.h \
+ /usr/include/c++/16.1.1/ext/numeric_traits.h \
+ /usr/include/c++/16.1.1/bits/stl_pair.h \
+ /usr/include/c++/16.1.1/bits/move.h \
+ /usr/include/c++/16.1.1/bits/utility.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator_base_types.h \
+ /usr/include/c++/16.1.1/bits/iterator_concepts.h \
+ /usr/include/c++/16.1.1/bits/ptr_traits.h \
+ /usr/include/c++/16.1.1/bits/ranges_cmp.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/16.1.1/bits/concept_check.h \
+ /usr/include/c++/16.1.1/debug/assertions.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator.h /usr/include/c++/16.1.1/new \
+ /usr/include/c++/16.1.1/bits/exception.h \
+ /usr/include/c++/16.1.1/bits/new_except.h \
+ /usr/include/c++/16.1.1/bits/stl_construct.h \
+ /usr/include/c++/16.1.1/debug/debug.h \
+ /usr/include/c++/16.1.1/bits/predefined_ops.h \
+ /usr/include/c++/16.1.1/bits/stl_function.h \
+ /usr/include/c++/16.1.1/backward/binders.h /usr/include/c++/16.1.1/bit \
+ /usr/include/c++/16.1.1/bits/range_access.h \
+ /usr/include/c++/16.1.1/optional /usr/include/c++/16.1.1/exception \
+ /usr/include/c++/16.1.1/bits/exception_ptr.h \
+ /usr/include/c++/16.1.1/bits/cxxabi_init_exception.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stddef.h \
+ /usr/include/c++/16.1.1/typeinfo \
+ /usr/include/c++/16.1.1/bits/hash_bytes.h \
+ /usr/include/c++/16.1.1/bits/nested_exception.h \
+ /usr/include/c++/16.1.1/bits/enable_special_members.h \
+ /usr/include/c++/16.1.1/bits/functional_hash.h \
+ /usr/include/c++/16.1.1/bits/invoke.h
